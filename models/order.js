d1f10185e93351e699a3fe2f37bf196df2298e51
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2018-03-19 12:00:00 +0530

    added status, date and time attribute to order

--- models/order.js
+++ models/order.js
@@ -4,6 +4,9 @@ var Schema = mongoose.Schema;
 var schema = new Schema({
     user: {type: Schema.Types.ObjectId, ref: 'User'},
     cart: {type: Object, required: true},
+    date: {type: String, required: true},
+    time: {type: String, required: true},
+    status: {type: String, required: true},
     address: {type: String, required: true},
     name: {type: String, required: true},
     paymentId: {type: String, required: true}
